5a13c53bb2d25934456efffd3f31fae747442626
yuriilychak <yurii.lychak@gmail.com>
2018-12-04 10:23:49 +0200

    Add mangle props

--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,65 @@ module.exports = {
             sourceMap: true,
             uglifyOptions: {
                 compress: true,
-                ie8: false
-            }
+                ie8: false,
+                mangle: true
+            },
+            minify(file, sourceMap) {
+                // https://github.com/mishoo/UglifyJS2#minify-options
+                const uglifyJsOptions = {
+                    compress: true,
+                    ie8: false,
+                    mangle: true
+                };
+
+                function replaceAll(target, search, replacement) {
+                    return target.split(search).join(replacement);
+                }
+
+                const regExMethod = /(?:^|\W)key: "_(\w+)(?!\w)"/g;
+                const regExProperty = /(?:^|\W)this._(\w+)(?!\w)/g;
+                const prefixMethod = "pm";
+                const prefixVariable = "pv";
+                //const found = file.match(regex);
+                let resStr, found, foundCount, i;
+                for (let key in file) {
+                    resStr = file[key];
+                    found = resStr.match(regExMethod);
+                    foundCount = found.length;
+
+                    for (i = 0; i < foundCount; ++i) {
+                        resStr = replaceAll(resStr, found[i].replace(' key: "', "").replace('"', ""), prefixMethod + i.toString(16));
+                    }
+
+                    found = resStr.match(regExProperty);
+                    foundCount = found.length;
+
+                    for (i = 0; i < foundCount; ++i) {
+                        found[i] = found[i].split(".")[1];
+                    }
+
+                    found = found.sort().filter(function(item, pos, ary) {
+                        return !pos || item !== ary[pos - 1];
+                    });
+
+                    foundCount = found.length;
+
+                    for (i = 0; i < foundCount; ++i) {
+                        resStr = replaceAll(resStr, found[i], prefixVariable + i.toString(16));
+                    }
+
+                    file[key] = resStr;
+                }
+
+
+                if (sourceMap) {
+                    uglifyJsOptions.sourceMap = {
+                        content: sourceMap,
+                    };
+                }
+
+                return require('terser').minify(file, uglifyJsOptions);
+            },
         })]
     },
 
